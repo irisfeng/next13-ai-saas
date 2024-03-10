3ec32ab23fceadf347b41b4e45b185341529658e
irisfeng <tonifang@189.cn>
2024-03-10 04:37:33 +0000

    navbar updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,17 @@
 import { UserButton } from "@clerk/nextjs";
 
 import { MobileSidebar } from "@/components/mobile-sidebar";
-import { getApiLimitCount } from "@/lib/api-limit";
+import { getApiLimitCount, getGpt3ApiLimitCount } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
 
 const Navbar = async () => {
   const apiLimitCount = await getApiLimitCount();
+  const gpt3ApiLimitCount = await getGpt3ApiLimitCount();
   const isPro = await checkSubscription();
 
   return ( 
     <div className="flex items-center p-4">
-      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} gpt3ApiLimitCount={gpt3ApiLimitCount} />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
