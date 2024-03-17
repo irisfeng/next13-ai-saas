43c0683beff5f75e1dddc815b5044fadd8773f8f
irisfeng <tonifang@189.cn>
2024-03-17 05:22:30 +0000

    little fix

--- app/(dashboard)/(routes)/writing/page.tsx
+++ app/(dashboard)/(routes)/writing/page.tsx
@@ -36,7 +36,7 @@ const writingAssistant = () => {
           title="提示助手"
           showBadge={1}
           badgeText='GPT4驱动'
-          description="文生图提示语生成 "
+          description="文生图提示生成 "
           icon={Pen}
           iconColor="text-orange-700"
           bgColor="bg-orange-700/10"
@@ -63,7 +63,7 @@ const writingAssistant = () => {
                     value={input}
                     disabled={status !== 'awaiting_message'}
                     onChange={handleInputChange}
-                    placeholder="输入中文/英文，复制生成的英文提示"
+                    placeholder="输入内容，复制生成的英文提示"
                   />
                   <Button variant={'send'} type="submit" size="icon">
                     <Send />
